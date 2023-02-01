3e539458d683c059a287f014905252e364f442a3
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-02-01 13:45:10 -0600

    Feedback

--- model.mjs
+++ model.mjs
@@ -1,7 +1,4 @@
 import noteFactory from "./presenter.mjs";
-import { zeroAdder } from "./auxiliaryFunctions.mjs";
-import entryReader from "./auxiliaryFunctions.mjs";
-import noteCommand from "./command.mjs";
 
 class Model {
   constructor() {
@@ -13,90 +10,75 @@ class Model {
   }
 
   indexInitialization() {
-    const visited = localStorage.getItem("0");
-    if (!!visited) {
-      // When the user refresh the page
-      noteCommand.reloadCommand();
-    } else {
-      // When the user opens the page for the first time
-      localStorage.setItem("i", "1");
-      // localStorage.setItem("0", "visited");
-    }
+    localStorage.setItem("i", "1");
   }
 
   modelGetItem(id) {
     return localStorage.getItem(id);
   }
 
-  searchHandler(array) {
-    for (let elmnt of Object.entries(localStorage)) {
-      // Part 1: All the notes are deleted by default the matches
-      //         will create the note.
-      if (elmnt[0] !== "0" && elmnt[0] !== "i") {
-        let noteToDelete = document.getElementById(elmnt[0]);
-        if (noteToDelete) {
-          document.body.removeChild(noteToDelete);
-        }
-      }
-    }
+  getItemJSON(id) {
+    let entries = localStorage.getItem("entries");
+    let parsedEntries = JSON.parse(entries);
+    return parsedEntries[id];
+  }
 
-    // Part 2: When the input is empty, all the notes
-    //         will be shown.
-    if (array.length === 0) {
-      for (let elmnt of Object.entries(localStorage)) {
-        if (elmnt[0] !== "0" && elmnt[0] !== "i") {
-          noteFactory(elmnt[0], elmnt[1]);
-        }
-      }
-    }
+  searchHandler(text) {
+    let entries = localStorage.getItem("entries");
+    let parsedEntries = JSON.parse(entries);
 
-    // Part 3: The code will create only notes that match the
-    //         input.
-    for (let elmnt of Object.entries(localStorage)) {
-      if (elmnt[0] !== "0" && elmnt[0] !== "i") {
-        let text = entryReader(elmnt[1]).text.toUpperCase();
-        text = text.split(" ");
-        for (let word of array) {
-          // console.log(text.indexOf(word.toUpperCase()));
-          if (text.indexOf(word.toUpperCase()) !== -1) {
-            noteFactory(elmnt[0], elmnt[1]);
-          }
-        }
+    // Part 1: The code delete all notes
+    // (Notes will be created if match the search)
+    (function () {
+      let notes = Array.from(document.getElementsByClassName("note"));
+      notes.forEach((element) => element.remove());
+    })();
+
+    // Part 2: When the input is empty, all the notes will be shown.
+    for (let elmnt of Object.entries(parsedEntries)) {
+      let regex = new RegExp(text, "gi");
+      if (regex.test(elmnt[1].content)) {
+        noteFactory(elmnt[0], elmnt[1]);
       }
     }
   }
 
   undoAction(command) {
-    let action = command.action;
-    let id = command.id;
+    try {
+      let action = command.action;
+      let id = command.id;
+      let entries = localStorage.getItem("entries");
+      entries = JSON.parse(entries);
 
-    // Handle the creation event:
-    if (action === "noteCreation") {
-      let noteToDelete = document.getElementById(id);
-
-      document.body.removeChild(noteToDelete);
-      localStorage.removeItem(id);
-    }
-    // Handle the modification:
-    else if (action === "noteModification" || action === "noteMovement") {
-      // 1) The code modifies the local storage, with the
-      //    values provided in the command.
-      // console.log(id, command.entry);
-      localStorage.setItem(id, command.entry);
-      // 2) The code deletes the "old" element from the DOM
-      //    ("old" => element before de -undo- action)
-      let noteToDelete = document.getElementById(id);
-      document.body.removeChild(noteToDelete);
-      // 3) The code creates a new element with the same id
-      //    than the one removed in step 2, but with a new
-      //    value for the text.
-      noteFactory(id, command.entry);
-    }
-    // Handle the deletion:
-    else if (action === "noteDeletion") {
-      localStorage.setItem(id, command.entry);
-      noteFactory(id, command.entry);
-    }
+      // Handle the creation event:
+      if (action === "noteCreation") {
+        let noteToDelete = document.getElementById(id);
+        document.body.removeChild(noteToDelete);
+        delete entries[id];
+        localStorage.setItem("entries", JSON.stringify(entries));
+      }
+      // Handle the modification:
+      else if (action === "noteModification" || action === "noteMovement") {
+        // 1) The code modifies the local storage, with the
+        //    values provided in the command.
+        entries[id] = command.entry;
+        localStorage.setItem("entries", JSON.stringify(entries));
+        // 2) The code deletes the "old" element from the DOM
+        //    ("old" => element before de -undo- action)
+        let noteToDelete = document.getElementById(id);
+        document.body.removeChild(noteToDelete);
+        // 3) The code creates a new element with the same id
+        //    than the one removed in step 2, but with a new
+        //    value for the text.
+        noteFactory(id, command.entry);
+      }
+      // Handle the deletion:
+      else if (action === "noteDeletion") {
+        entries[id] = command.entry;
+        localStorage.setItem("entries", JSON.stringify(entries));
+        noteFactory(id, command.entry);
+      }
+    } catch (TypeError) {}
   }
 
   commandHandler(command) {
@@ -111,71 +93,90 @@ class Model {
       // The position:
       let top = 0;
       let left = 400;
-      // To format the position:
-      top = zeroAdder(top);
-      left = zeroAdder(left);
 
       // The dates:
       let creationDate = new Date();
       creationDate = String(creationDate).substring(0, 24);
       let modificationDate = creationDate;
 
+      // ----------Start: JSON format----------
       // Creation of the entry in the localStorage
-      let entry = left + top + creationDate + modificationDate;
-
-      let entryJson = {
+      let entryJSON = {
         positionX: left,
         positionY: top,
         creationDate: creationDate,
         modificationDate: modificationDate,
+        content: "",
       };
-
-      // To add the entry to the model
-      // Model.newEntry(JSON.stringify(entryJson));
-      // console.log(JSON.stringify(entryJson));
-
-      localStorage.setItem(index, entry);
-
+      // There are 2 options:
+      // 1) This is the first note created:
+      if (!localStorage.getItem("entries")) {
+        // The code creates the -entries- entry in the localStorage
+        let entries = {};
+        entries[index] = entryJSON;
+        localStorage.setItem("entries", JSON.stringify(entries));
+      }
+      // 2) There are previously created notes:
+      else {
+        // The code uses the -entries- stored in the localStorage
+        // to retrive the information contained there:
+        let retrieveEntries = localStorage.getItem("entries");
+        let entries = JSON.parse(retrieveEntries);
+        // The code updates the -entries- entry in the localStorage:
+        entries[index] = entryJSON;
+        localStorage.setItem("entries", JSON.stringify(entries));
+      }
+      // ---------- End: JSON format ----------
       // Now the code creates the note
-      noteFactory(index, entry);
+      noteFactory(index, entryJSON);
     }
     // Handle the deletion:
     else if (action === "noteDeletion") {
+      // Remove the note from the DOM:
       let noteToDelete = document.getElementById(command.id);
       document.body.removeChild(noteToDelete);
-      localStorage.removeItem(command.id);
+
+      // Remove the note from the local storage
+      let retrieveEntries = localStorage.getItem("entries");
+      let entries = JSON.parse(retrieveEntries);
+      delete entries[command.id];
+      localStorage.setItem("entries", JSON.stringify(entries));
     }
     // Handle the modification:
     else if (action === "noteModification") {
-      let information = entryReader(command.entry);
       let modificationDate = new Date();
       modificationDate = String(modificationDate).substring(0, 24);
-      let newEntry =
-        information.coordinates[0] +
-        information.coordinates[1] +
-        information.creationDate +
-        modificationDate +
-        information.text;
 
-      localStorage.setItem(command.id, newEntry);
+      // ----------Start: JSON format----------
+      // The code uses the -entries- stored in the localStorage
+      // to retrive the information contained there:
+      let retrieveEntries = localStorage.getItem("entries");
+      let entries = JSON.parse(retrieveEntries);
+      // // The code updates the -entries- entry in the localStorage:
+      entries[command.id].modificationDate = modificationDate;
+      entries[command.id].content = command.entry.content;
+      localStorage.setItem("entries", JSON.stringify(entries));
+      // // ---------- End: JSON format ----------
     }
     // Handle the movement:
     else if (action === "noteMovement") {
-      let entryStoraged = localStorage.getItem(command.id);
-      let information = entryReader(command.entry);
-      let newEntry =
-        information.coordinates[0] +
-        information.coordinates[1] +
-        entryStoraged.substring(10, entryStoraged.length);
-      localStorage.setItem(command.id, newEntry);
+      // The code has to update the entries (to reflect the move)
+      // 1) Extract the entries from the local Storage:
+      let entries = localStorage.getItem("entries");
+      // 2) Parse the information:
+      entries = JSON.parse(entries);
+      // 3) Update the information:
+      entries[command.id] = command.entry;
+      // 4) Saves the updated entries in the local storage
+      localStorage.setItem("entries", JSON.stringify(entries));
     }
 
     // Handle the page refresh:
     else if (action === "pageRefresh") {
-      for (let elmnt of Object.entries(localStorage)) {
-        if (elmnt[0] !== "0" && elmnt[0] !== "i") {
-          noteFactory(elmnt[0], elmnt[1]);
-        }
+      let entries = localStorage.getItem("entries");
+      entries = JSON.parse(entries);
+      for (let elmnt of Object.entries(entries)) {
+        noteFactory(elmnt[0], elmnt[1]);
       }
     }
   }
